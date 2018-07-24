f1461db9a241219904aa0793e51200f82295fe5f
myroncj <myroncj@gmail.com>
2018-07-24 15:22:17 +0530

    templates: Added myMap

--- AnomalyCheck/views.py
+++ AnomalyCheck/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
@@ -7,3 +9,6 @@ from django.http import HttpResponse
 def show(request):
     name = "Myron CJ"
     return HttpResponse("My Name is " + str(name))
+
+def showMap(request):
+    return render_to_response('myMap.html')
