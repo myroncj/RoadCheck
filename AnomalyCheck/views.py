5f6b761583284b5847678e9250602f4a5fd37313
myroncj <myroncj@gmail.com>
2018-07-24 10:40:32 +0530

    Heroku Init

---
+++ AnomalyCheck/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def show(request):
+    name = "Myron CJ"
+    return HttpResponse("My Name is " + str(name))
+h
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/myroncj/PycharmProjects/RoadCheck/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
